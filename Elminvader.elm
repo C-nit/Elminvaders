1cda71cbbffeaed5b60d5c4bfabacd38b7393e0f
C-nit <joriscadow@gmail.com>
2015-08-21 13:05:14 +0200

    some formatting

--- Elminvader.elm
+++ Elminvader.elm
@@ -13,18 +13,22 @@ elminvdir = "C:/Users/Zenit/Desktop/ElmInvaders"
 
 --corrects 0,0 of coordinate system of collage vs mouseposition
 corrzero : Int -> Int -> Int
-corrzero a size =  a - size //2
+corrzero a size =
+    a - size // 2
 
 ----------------------------------------------Movements
 --moves form to x, y
-movetoxy : Int -> Int -> Form -> Form
-movetoxy x y = move (toFloat x, toFloat y) --(10.0* toFloat x, 10.0* toFloat y)
+moveToXY : Int -> Int -> Form -> Form
+moveToXY x y =
+    move (toFloat x, toFloat y) --(10.0* toFloat x, 10.0* toFloat y)
 
-movetox : Int -> Form -> Form
-movetox x = moveX (toFloat x)
+moveToX : Int -> Form -> Form
+moveToX x =
+    moveX (toFloat x)
 
-movetoy : Int -> Form -> Form
-movetoy y = moveY (toFloat y)
+movetoY : Int -> Form -> Form
+movetoY y =
+    moveY (toFloat y)
 
 --Element to From (forms can be moved, elements not)
 --toForm : Element -> Form
@@ -33,32 +37,35 @@ movetoy y = moveY (toFloat y)
 ----------------------------------------------Forms
 
 chugle : Form
-chugle = filled Color.red (circle 5.0)
+chugle =
+    filled Color.red (circle 5.0)
 
 --Form moving to x y   --mousepos.y is inverted
-chuglexy : ( Int, Int) -> ( Int, Int)  -> Form
-chuglexy (w, h) (x, y) = movetoxy (corrzero x w) -(corrzero y h) chugle
+chuglexy : (Int, Int) -> (Int, Int) -> Form
+chuglexy (w, h) (x, y) =
+    moveToXY (corrzero x w) -(corrzero y h) chugle
 
 invader : Form
-invader = toForm (image 30 30 ( elminvdir ++ "/spaceinvaders.jpg" ))
+invader =
+    toForm (image 30 30 (elminvdir ++ "/spaceinvaders.jpg"))
 
 --invader moving to x at y
-invaderx : Int -> ( Int, Int) -> Form
-invaderx x (w, h) = movetoxy (corrzero x w) (-h//2 + 15) invader -- -
+invaderx : Int -> (Int, Int) -> Form
+invaderx x (w, h) =
+    moveToXY (corrzero x w) (-h // 2 + 15) invader -- -
 
-timestep : Float -> Int
-timestep = floor inSeconds(every 1 )
+formlist : (Int, Int) -> (Int, Int) -> Time -> List Form
+formlist (w, h) (x, y) t =
+    [ chuglexy (w, h)  (x, y)
+    , invaderx x (w, h)
+    ]
 
-invaderstep : Form
-invaderstep  = movetoy (15*timestep) invader
-
-formlist : ( Int, Int) -> ( Int, Int)  -> List Form
-formlist (w, h) (x, y) = [ chuglexy (w, h)  (x, y), invaderx x (w, h), invaderstep]
-
-view : (Int, Int) -> ( Int, Int) ->  Element   -- { x : Int, y : Int}
+view : (Int, Int) -> (Int, Int) -> Time -> Element   -- { x : Int, y : Int}
 --view (x, y) = text ((toString x) ++ ", "  ++ (toString y))   --mauspo anzeigen
-view (w, h) (x, y) = collage w h (formlist (w, h) (x, y) )
+view (w, h) (x, y) t =
+    collage w h (formlist (w, h) (x, y) t)
 
 
 main : Signal Element
-main = Signal.map2 view Window.dimensions Mouse.position --Keyboard.arrows
+main =
+    Signal.map3 view Window.dimensions Mouse.position (fps 30) --Keyboard.arrows
