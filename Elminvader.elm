f68a9fded84f249644486abe0609190629038ae2
C-nit <joriscadow@gmail.com>
2015-08-24 15:08:32 +0200

    clean up + ready for more
    move parts and give commented titles of sections
    
    delete leftover code from change to foldp
    
    include view-functions and forms for shots

--- Elminvader.elm
+++ Elminvader.elm
@@ -9,12 +9,30 @@ import Color
 import Window
 import Debug
 
---corrects 0,0 of coordinate system of collage vs mouseposition
-corrZero : (Int, Int) -> (Int, Int) -> (Int, Int)
-corrZero (x, y) (w, h) =
-    (x - w // 2 , -y + h //2 )
+
+----------------------------------------------Movements
+
+--moves form to x, y
+moveToXY : Int -> Int -> Form -> Form
+moveToXY x y =
+    move (toFloat x, toFloat y) --(10.0* toFloat x, 10.0* toFloat y)
+
+----------------------------------------------Forms
+
+shot : Form
+shot =
+    filled Color.red (circle 5.0)
+
+shot2 : Form
+shot2 =
+    filled Color.orange (circle 5.0)
+
+invaderForm : Form
+invaderForm =
+    toForm (image 40 30 "images/Ship.png")
+
 ----------------------------------------------Gamestate
-{--}
+
 type Livelyness = Dead | Alive
 
 type alias Dimensions = (Int, Int)
@@ -43,25 +61,44 @@ type alias DefenderFire = --ExistantAt
     , livelyness : Livelyness
     }
 
+type alias InvaderFire = --ExistantAt
+    { x : Int
+    , y : Int
+    , livelyness : Livelyness
+    }
+
 type alias GameState =
     { defender : Defender
     , invaders : List Invader
---    , defenderFire: DefenderFire
---    , invaderFire : List InvaderFire
+    , defenderFires: List DefenderFire
+    , invaderFires : List InvaderFire
     , dim : Dimensions
     }
-
---}
 
 --Default Gamestate
 gameStart : GameState
 gameStart =
     { defender = Defender 0 -135 Alive   --(Window.height // 2 + 15)
-    , invaders = [ Invader -20 0 Alive]
+    , invaders = [ Invader -20 60 Alive]
+    , defenderFires = [ DefenderFire 120 0 Dead]
+    , invaderFires = [ InvaderFire 120 40 Dead]
     , dim = (400, 300)
     }
 
---All Signals !! order is of importance, left one
+--current gameState
+gameState : Signal GameState
+gameState =
+    Signal.foldp updateGame gameStart mergedSignals
+
+
+----------------------------------------------Signals + Update
+
+--corrects 0,0 and y direction of coordinate system of collage vs mouseposition
+corrZero : (Int, Int) -> (Int, Int) -> (Int, Int)
+corrZero (x, y) (w, h) =
+    (x - w // 2 , -y + h //2 )
+
+--All Signals !! order is of importance, left one has priority
 type Update =   DimDelta (Int, Int)
                 | MouseMove (Int, Int)
                 | TimeDelta Time
@@ -77,12 +114,7 @@ mergedSignals =
         , Signal.map (always Tick) (Time.every Time.second)
         ]
 
---current gameState
-gameState : Signal GameState
-gameState =
-    Signal.foldp updateGame gameStart mergedSignals
-
--- UNFINISHED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- UNFINISHED!!
 updateGame : Update -> GameState -> GameState
 updateGame update state =
     case update of
@@ -98,20 +130,35 @@ updateGame update state =
         Click -> state
         Tick -> state
 
-
-
+---------------------------------------------- View functions
 
 viewDefender : Defender -> Form
 viewDefender defender =
-    defenderXY (Debug.watch "Defender" defender.x) (Debug.watch "Defendery" defender.y)
+    moveToXY defender.x defender.y invaderForm --Debug.watch "Defendery"
 
 viewInvader : Invader -> Form
 viewInvader invader =
-    invaderXY invader.x invader.y
+    moveToXY invader.x invader.y invaderForm
 
 viewInvaders : List Invader  -> List Form
 viewInvaders invaders =
     List.map viewInvader invaders
+
+viewDefenderFire : DefenderFire -> Form
+viewDefenderFire defenderFire =
+    moveToXY defenderFire.x defenderFire.y shot
+
+viewDefenderFires : List DefenderFire -> List Form
+viewDefenderFires defenderFires =
+    List.map viewDefenderFire defenderFires
+
+viewInvaderFire : InvaderFire -> Form
+viewInvaderFire invaderFire =
+    moveToXY invaderFire.x invaderFire.y shot2
+
+viewInvaderFires : List InvaderFire -> List Form
+viewInvaderFires invaderFires =
+    List.map viewInvaderFire invaderFires
 
 viewGame : GameState -> Element
 viewGame state =
@@ -119,92 +166,12 @@ viewGame state =
         (fst state.dim)
         (snd state.dim)
         (viewDefender state.defender
-            :: viewInvaders state.invaders)
+            :: viewInvaders state.invaders
+            |> List.append (viewDefenderFires state.defenderFires)
+            |> List.append (viewInvaderFires state.invaderFires)
+            )
 
-
-
-
-
+---------------------------------------------- Main
 
 main : Signal Element
 main = Signal.map viewGame gameState
-
-----------------------------------------------Movements
---moves form to x, y
-moveToXY : Int -> Int -> Form -> Form
-moveToXY x y =
-    move (toFloat x, toFloat y) --(10.0* toFloat x, 10.0* toFloat y)
-
-moveToX : Int -> Form -> Form
-moveToX x =
-    moveX (toFloat x)
-
-movetoY : Int -> Form -> Form
-movetoY y =
-    moveY (toFloat y)
-
---Element to From (forms can be moved, elements not)
---toForm : Element -> Form
---image : Int -> Int -> String -> Element
---floor --roun
-----------------------------------------------Forms
-
-
-invader : Form
-invader =
-    toForm (image 40 30 "images/Ship.png")
-
---invader moving to x at y
-defenderXY : Int -> Int -> Form
-defenderXY x y=
-    moveToXY x y invader
-
-invaderXY : Int -> Int -> Form
-invaderXY x y =
-    moveToXY x y invader
-
-{-}
-chugle : Form
-chugle =
-    filled Color.red (circle 5.0)
-
---Form moving to x y   --mousepos.y is inverted
-chugleXY : (Int, Int) -> (Int, Int) -> Form
-chugleXY (w, h) (x, y) =
-    moveToXY (corrZero x w) -(corrZero y h) chugle
-
-invader : Form
-invader =
-    toForm (image 40 30 "images/Ship.png")
-
---invader moving to x at y
-defenderXY : Int -> (Int, Int) -> Form
-defenderXY x (w, h) =
-    moveToXY (corrZero x w) (-h // 2 + 15) invader
-
-invaderXY : Int -> Int -> (Int, Int) -> Form
-invaderXY x y (w, h) =
-    moveToXY (corrZero x w) (corrZero y h) invader
-
-invaderStep : Time -> Form
-invaderStep t =
-    movetoY -10 invader
-
-
-formlist : (Int, Int) -> (Int, Int) -> Time -> List Form
-formlist (w, h) (x, y) t =
-    [ chugleXY (w, h)  (x, y)
-    , defenderXY x (w, h)
-    , invaderStep t
-    ]
-
-view : (Int, Int) -> (Int, Int) -> Time -> Element   -- { x : Int, y : Int}
---view (x, y) = text ((toString x) ++ ", "  ++ (toString y))   --mauspo anzeigen
-view (w, h) (x, y) t =
-    collage w h
-    <| formlist (w, h) (x, y) t
---}
-
---main : Signal Element
---main =
---    Signal.map3 view Window.dimensions Mouse.position (fps 30 ) --Keyboard.arrows
